d255e8e8a9e4f4b448e2cbfe5595a97415f5b90a
Saumya Jain <saumyain23@gmail.com>
2024-04-06 17:05:25 +0530

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './components/Nav';
-import Catalog from './pages/Catalog';
 import Details from './pages/Details';
 import Gainers from './containers/Gainers';
 
